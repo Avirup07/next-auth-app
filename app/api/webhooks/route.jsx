d548855ff5829be723f2fc4d8c88ec60fcf1588c
Avirup07 <sarmisthapal29@gmail.com>
2025-06-23 14:34:54 +0530

    Add MongoDB and Mongoose to save data from Clerk

--- app/api/webhooks/route.jsx
+++ app/api/webhooks/route.jsx
@@ -6,15 +6,45 @@ export async function POST(req) {
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
+    const { id } = evt?.data;
+    const eventType = evt?.type;
     console.log(
       `Received webhook with ID ${id} and event type of ${eventType}`
     );
     console.log("Webhook payload:", evt.data);
 
-    if (eventType === "user.created") {
-        console.log("New user created");
+    switch (eventType) {
+      case "user.created":
+      case "user.updated":
+        // Handle user creation or update
+        const { first_name, last_name, email_addresses, image_url, username } = evt?.data;
+        try {
+          await createOrUpdateUser(
+            evt.data.id,
+            first_name,
+            last_name,
+            email_addresses,
+            image_url,
+            username
+          );
+          return new Response("User created or updated", { status: 200 });
+        } catch (error) {
+          console.error("Error creating or updating user:", error);
+          return new Response("Error processing user data", { status: 500 });
+        }
+        break;
+      case "user.deleted":
+        const { id } = evt?.data;
+        try {
+          await deleteUser(id);
+          return new Response("User deleted", { status: 200 });
+        } catch (error) {
+          console.error("Error deleting user:", error);
+          return new Response("Error processing user deletion", { status: 500 });
+        }
+        break;
+      default:
+        console.warn(`Unhandled event type: ${eventType}`);
     }
 
     return new Response("Webhook received", { status: 200 });
